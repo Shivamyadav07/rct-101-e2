426a8b12ff4a84aada6a6d620a48807a0bec48fd
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-05-30 16:41:28 +0530

    Update Pagination.jsx

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,7 +7,7 @@ const Pagination = ({handlepage,handlelimit,page}) => {
     <ButtonGroup marginTop={"20px"} marginLeft={"400px"} >
       <Button data-cy="pagination-first-button" onClick={()=>handlepage(1)}>first</Button>
       <Button data-cy="pagination-previous-button" onClick={()=>handlepage(page-1)}>previous</Button>
-      <Select data-cy="pagination-limit-select" onSelect={(e)=>console.log(e.target.value)}>
+      <Select data-cy="pagination-limit-select" onChange={(e)=>handlelimit(e.target.value)}>
         <option data-cy="pagination-limit-3" value={3}>3</option>
         <option data-cy="pagination-limit-6" value={6}>6</option>
         <option data-cy="pagination-limit-9" value={9}>9</option>
